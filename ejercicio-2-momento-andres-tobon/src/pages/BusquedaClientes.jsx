c20c6b56667cae8a8027ad33ceefcee8d0f06374
harbby2024 <harbby.a.t@gmail.com>
2025-05-16 04:46:44 -0500

    nuevos cambios

--- ejercicio-2-momento-andres-tobon/src/pages/BusquedaClientes.jsx
+++ ejercicio-2-momento-andres-tobon/src/pages/BusquedaClientes.jsx
@@ -1,7 +1,43 @@
-function BusquedaCliente(){
+import { useEffect } from "react";
+import Swal from "sweetalert2";
+import './BusquedaClientes.css';
+import LogoMercadoLibre from "/mercado-libre-logo-1.png"
+
+function BusquedaCliente() {
+    
+  useEffect(() => {
+    const mostrarAlerta = async () => {
+      const { value: email } = await Swal.fire({
+        title: "Ingresa el correo del cliente",
+        input: "email",
+        inputLabel: "Correo electrónico",
+        inputPlaceholder: "ejemplo@correo.com",
+        customClass: {
+          title: "swal-title",
+          input: "swal-input",
+          popup: "swal-popup",
+          confirmButton: "swal-confirm-button"
+        }
+      });
+
+      if (email) {
+        Swal.fire(`Correo ingresado: ${email}`);
+      }
+    };
+
+    mostrarAlerta();
+  }, []);
+
+  return null; // No renderiza nada visible
+}
+
+function logoBienvenida(){
     return(
-        <div>BusquedaCliente</div>
-    );
+        <section className="logoBienvenida">
+                <h1>Buscar Cliente</h1>
+                <img src={LogoMercadoLibre}  alt="" srcset="" />
+        </section>
+    )
 }
 
 export default BusquedaCliente;
